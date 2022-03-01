eb15d75d16fd2dc9eb0b2a768116cc0b91a0671e
Bekmyrza <msn.10110922@gmail.com>
2022-03-02 00:30:05 +0600

    Сделал рефакторинг приложения, переписал функции и преобразовал их в методы объектов, добавил новые возможности в метод жанра, добавил новый метод для переключения видимости базы данных

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 // Задание на урок
 /*
-1) Первую часть задания повторить по уроку
+1) У нас есть рабочее приложение, состоящее из отдельных функций. Представьте, что 
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта
+personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-2) Создать функцию showMyDb, которая будет проверять свойство privat.
-Если стоит в позиции false - выводит главный объект программы
+2) Создать метод toggleVisybleMyDB, который при вызове будет проверять private. Если 
+оно false - он 
+переключает его в true. Если true - переключает в false. Протестировать вместе с showMyDB.
 
-3) Создать функцию writeYourGenres в которой пользователь будет
-3 раза отвечать на вопрос "Ваш любимый жанр под номером ${номер по порядку}". 
-Каждый ответ записыватся в массив данных genres
-
-P.S. Функции вызывать не обязательно
+3) В методе writeYourGenres запретить пользователю нажать кнопку "Отмена" или оставлять пустую строку.
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены -
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) это (название из массива) "
 */
+
+// Запрашиваем у зрителя количество просмотренних им фильмов
 let numberOfFilms;
 
 function start() {
@@ -28,81 +33,105 @@ const personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
+    privat: false,
 
-// writeYourGenres();
+    // Запрашиваем у зрителя поледний просмотренний его фильм
+    // и его оценку к этому фильму
+    remembersMyFilms() {
+        let movies = 0;
+        while (movies < 2) {
+            const a = prompt("Один из последних просмотренных фильмов?", ""),
+                b = +prompt("На сколько оцените этот фильм?", "");
 
-function remembersMyFilms() {
-    let movies = 0;
-    while (movies < 2) {
-        const a = prompt("Один из последних просмотренных фильмов?", ""),
-            b = +prompt("На сколько оцените этот фильм?", "");
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                this.movies[a] = b;
+                console.log('done');
+            } else {
+                movies--;
+            }
 
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
+            movies++;
+        }
+    },
+
+    // Оценка зрителя на киномана
+    detectPersonalLevel() {
+        if (personalMovieDB.count < 10) {
+            alert('Просмотрено довольно мало фильмов.');
+        } else if (personalMovieDB.count < 30) {
+            alert('Вы классический зритель.');
+        } else if (personalMovieDB.count >= 30) {
+            alert('Вы киноман.');
         } else {
-            movies--;
+            alert('Произошла ошибка.');
+        }
+    },
+
+    // Показываем БД, если БД видимая 
+    // Так тоже правильно
+    // function showMyDb() {
+    //     if (personalMovieDB.privat == false) {
+    //         console.log(personalMovieDB);
+    //     }
+    // }
+
+    showMyDb(hidden) {
+        if (!hidden) {
+            console.log(this);
+        }
+    },
+
+    // Запрашиваем у пользователя любимый жанр фильма
+    // Мой вариант
+    // function writeYourGenres() {
+    //     let genre;
+
+    //     for (let i = 1; i <= 3; i++) {
+    //         genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+
+    //         let copy = genre;
+
+    //         if (genre !== null && genre !== '') {
+    //             personalMovieDB.genres.push(copy);
+    //         }
+    //     }
+    // }
+
+    // Решение
+    writeYourGenres() {
+        for (let i = 1; i <= 3; i++) {
+            const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+
+            if (genre == null || genre == '') {
+                i--;
+            } else {
+                this.genres[i - 1] = genre;
+            }
         }
 
-        movies++;
-    }
-}
+        this.genres.forEach((element, index) => {
+            console.log(`Любимый жанр ${index + 1} это ${element}`);
+        });
+    },
+
+    // Преключатель видимости БД
+    toggleVisybleMyDB() {
+        if (this.privat === false) {
+            this.privat = true;
+        } else {
+            this.privat = false;
+        }
+    },
+};
+
+// Вызываем методы
 
 // remembersMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert('Просмотрено довольно мало фильмов.');
-    } else if (personalMovieDB.count < 30) {
-        alert('Вы классический зритель.');
-    } else if (personalMovieDB.count >= 30) {
-        alert('Вы киноман.');
-    } else {
-        alert('Произошла ошибка.');
-    }
-}
-
 // detectPersonalLevel();
 
-// Так тоже правильно
-// function showMyDb() {
-//     if (personalMovieDB.privat == false) {
-//         console.log(personalMovieDB);
-//     }
-// }
+personalMovieDB.showMyDb(personalMovieDB.privat);
 
-function showMyDb(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
+personalMovieDB.writeYourGenres();
 
-showMyDb(personalMovieDB.privat);
-
-// Мой вариант
-// function writeYourGenres() {
-//     let genre;
-
-//     for (let i = 1; i <= 3; i++) {
-//         genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-
-//         let copy = genre;
-
-//         if (genre !== null && genre !== '') {
-//             personalMovieDB.genres.push(copy);
-//         }
-//     }
-// }
-
-// Решение
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-
-        personalMovieDB.genres[i - 1] = genre;
-    }
-}
-
-writeYourGenres();
+personalMovieDB.toggleVisybleMyDB();
